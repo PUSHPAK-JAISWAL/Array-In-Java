3e7d27d6a222404386ae159e4cb225b276f77025
PUSHPAK-JAISWAL <149317014+PUSHPAK-JAISWAL@users.noreply.github.com>
2023-11-13 20:32:01 +0530

    Add files via upload

---
+++ P1.java
@@ -0,0 +1,13 @@
+public class P1
+{
+    public static void main(String args[])
+    {
+        int nums[] = {0,1,2,3,4,5,6,7,8,9};
+        int a = 8;
+        System.out.println(a);
+        for(int i=0;i<10;i++)
+        {
+            System.out.println(nums[i]);
+        }
+    }
+}
---
+++ P2.java
@@ -0,0 +1,15 @@
+public class P2
+{
+    public static void main(String args[])
+    {
+        int nums[] = new int[4];
+        nums[0]=5;
+        nums[1]=4;
+        nums[2]=3;
+        nums[3]=2;
+        System.out.println(nums[3]);
+        System.out.println(nums[2]);
+        System.out.println(nums[1]);
+        System.out.println(nums[0]);
+    }
+}
