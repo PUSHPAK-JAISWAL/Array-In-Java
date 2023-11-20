3f1b2c64531dbb7cf6e5e14e9107733d37f8b8c4
PUSHPAK-JAISWAL <149317014+PUSHPAK-JAISWAL@users.noreply.github.com>
2023-11-20 20:55:00 +0530

    This program is connected to each other

---
+++ P14.java
@@ -0,0 +1,13 @@
+// this is the object class for the class P14_1
+public class P14
+{
+    public int add(int add[])
+    {
+        int result = 0;
+        for(int n : add)
+        {
+            result += n;
+        }
+        return result;
+    }
+}
---
+++ P14_1.java
@@ -0,0 +1,10 @@
+// this is the program for the anonymous array
+public class P14_1
+{
+    public static void main(String args[])
+    {
+        P14 obj = new P14();
+        int result = obj.add(new int[]{ 2,3,4,5,6,7,8,9});
+        System.out.println(result);
+    }
+}
